bba538b3ee20d48d560968c56eaa204762b0590c
Gregory Bice <GregBice@udel.edu>
2018-04-18 18:32:15 -0400

    JUnit test added

---
+++ BlackjackFX/Lab4Starter-master/FlamingoBLL/src/test/java/pkgCore/PlayerTest.java
@@ -0,0 +1,30 @@
+package pkgCore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import pkgEnum.eGameType;
+
+public class PlayerTest {
+	
+	@Test
+	public void TestTable() {
+		Table table = new Table();
+		
+		Player p1 = new Player("Foo", 0 );
+		Player p2 = new Player("Bar", 1 );
+		
+		table.AddPlayerToTable( p1 );
+		table.AddPlayerToTable( p2 );
+		
+		assertEquals( table.GetPlayerFromTable( p1 ).getPlayerName(), "Foo" );
+		assertEquals( table.GetPlayerFromTable( p2 ).getPlayerName(), "Bar" );
+		
+		table.RemovePlayerFromTable( p2 );
+		
+		assertNull( table.GetPlayerFromTable( p2 ) );
+		
+		
+	}
+	
+}
